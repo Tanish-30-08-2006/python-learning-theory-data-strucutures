4f9d1f7f1ef61fa8baaed20f413e4c5cb421af05
Tanish <tanishsanghavi2@gmail.com>
2026-03-17 13:48:28 +0530

    feat: add important notes and conceptual points

---
+++ python_data_structures/imp_points.py
@@ -0,0 +1,31 @@
+'''''''''''I  
+S={} THEN type(s) = dictionary
+
+1=1.0 in python as 
+as python checks for values if they are equal then datatype doesnt matter
+so in set if i add 20 , '20',20.0 size will be 2 of 20 as a string and 20 as integer 20 and 20.0 are duplicates 
+
+s = {8, 7, 12, "Harry", [1,2]}
+
+List is mutable → you can change it
+Mutable objects do not have a fixed hash
+Therefore not hashable
+Hence cannot be stored in a set
+You cannot add lists to a set because lists are mutable and not hashable, while set elements must be immutable and hashable.
+
+
+TO DECLARE INT_MAX AND INT_MIN
+z=float('-inf') --> INT_MIN
+z=float('+inf')---> INT_MAX
+
+
+print("*",end="")
+    print() //python always ends a prnt statement with a new line so print() prints a new line
+    also , to PRINT IN SAME LINE WE USE print("data to print ",end="")
+
+
+---> TO ONLY GET OUTPUT IN N DECIMAL PLACES
+N=5.666609
+print(round(N,2)) 5.66-->OUTPUT
+
+'''''''''''
