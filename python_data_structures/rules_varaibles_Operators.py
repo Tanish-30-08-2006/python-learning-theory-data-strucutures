03e0a5f3487d9d34e231bea878b02963ef710c80
Tanish <tanishsanghavi2@gmail.com>
2026-03-17 13:49:32 +0530

    feat: add rules for variables and operator usage

---
+++ python_data_structures/rules_varaibles_Operators.py
@@ -0,0 +1,30 @@
+''' variable name cant start with a digit
+variable name cant start with a digtit and can only have characters numbers and underscores'''
+ 
+#1=1.0 in python as 
+#as python checks for values if they are equal then datatype doesnt matter
+#so in set if i add 20 , '20',20.0 size will be 2 of 20 as a string and 20 as integer 20 and 20.0 are duplicates 
+
+a=5
+
+bbb=6
+
+'''
+1sameer INVALID
+@SAMEER ALSO INVALID
+'''
+b=5>4
+c=6<4 #true =1
+print(b,c)
+b=b+5
+print(b)
+
+
+e=  True or False
+f= True and True
+print(e,f)
+
+
+
+
+
