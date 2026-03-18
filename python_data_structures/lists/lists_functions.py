ee9f5e7728474370361ce21374d914c02b2e6bd2
Tanish <tanishsanghavi2@gmail.com>
2026-03-18 17:50:19 +0530

    feat: implementation of common list functions and methods

---
+++ python_data_structures/lists/lists_functions.py
@@ -0,0 +1,26 @@
+print("list functions : ")
+list1=[5,4,3,2,1]
+print(list1.append(6))
+# NONE IS OUTPUT :  AS LIST.FUNCTIONS JUST ADD , REMOVE ,OR CHANGE THE LIST THEY DONT
+# RETURN A list AS OUTPUT UNLIKE strings
+print(list1)
+
+list1.sort()
+print(list1)
+
+list1.reverse()
+print(list1)
+
+list1.insert(3,55)
+print(list1)
+
+list1.pop(3)
+print(list1)    
+
+list1.remove(6)
+print(list1) 
+
+for i in list1:
+    print(i)
+ 
+
