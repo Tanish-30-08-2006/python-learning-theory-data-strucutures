c506c272e94e37ed92863bf5564f7388277db83a
Tanish <tanishsanghavi2@gmail.com>
2026-03-20 20:48:31 +0530

    feat: add logic for user input and f-string sum output

--- python_data_structures/sets and tuples/typefunc_typecasting_takinginput.py
+++ python_data_structures/sets and tuples/typefunc_typecasting_takinginput.py
@@ -12,3 +12,32 @@ c=int(c) #typecasting
 print(type(c))
 
 
+#Input
+a=input("enter number 1")
+b=input("enter number 2")#every Input without typecast is taken as string
+print(f"sum is  {a+b}")
+
+
+#converting to int 
+a=int(input("enter number 1"))
+b=int (input("enter number 2"))#every Input without typecast is taken as string
+print(f"sum is {a+b}")#taking input by f string method
+
+
+d=input("enter your name: ")
+print(f"good morning {d} ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
