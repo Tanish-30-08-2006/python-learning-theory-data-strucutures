f126c5debb313a77f08160b5136bb6d8dbded893
Tanish <tanishsanghavi2@gmail.com>
2026-03-20 20:48:26 +0530

    feat: add variable type checking and manual typecasting

---
+++ python_data_structures/sets and tuples/typefunc_typecasting_takinginput.py
@@ -0,0 +1,14 @@
+a="harry"
+print(type(a))
+
+b=1
+print(type(b))
+
+c="56" #inside double quotes is always a string 
+print(type(c))
+
+# c as 56 but type should be int not string
+c=int(c) #typecasting
+print(type(c))
+
+
