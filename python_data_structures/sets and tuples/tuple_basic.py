590079efb8cc99cba4ab89d8d3cbd6e31f58c4a4
Tanish <tanishsanghavi2@gmail.com>
2026-03-20 20:48:41 +0530

    feat: add basic tuple structure and usage

---
+++ python_data_structures/sets and tuples/tuple_basic.py
@@ -0,0 +1,10 @@
+# we need a datatype which is immutable(strings) and stores like a list(all datatypes)
+a=(1,2,3,4,5) #empty tuple ()
+print(type(a))
+
+#how to define a tuple with single element
+b=(1)
+print(type(b))
+c=(1,)
+print(type(c))
+
