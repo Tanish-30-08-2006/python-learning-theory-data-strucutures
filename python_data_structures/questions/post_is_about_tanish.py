c3964acaef182f30fcdca003f3ef97e50c7079dc
Tanish <tanishsanghavi2@gmail.com>
2026-03-22 22:02:55 +0530

    feat: implementation for post content keyword search

---
+++ python_data_structures/questions/post_is_about_tanish.py
@@ -0,0 +1,11 @@
+str1=input("enter your post: ")
+
+#covert to lowercase
+str2=str1.lower()#strings are immutant so so new lowercase string
+if("tanish" in str2):
+    print("yes")
+else:
+    print("no")
+    
+
+
