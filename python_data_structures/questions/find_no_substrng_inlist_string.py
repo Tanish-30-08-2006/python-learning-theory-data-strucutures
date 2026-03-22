5e5f3ca5a7854ecfb910c8f5a6b796ee7046b65f
Tanish <tanishsanghavi2@gmail.com>
2026-03-22 22:02:46 +0530

    feat: implement list membership search with dynamic split input

--- python_data_structures/questions/find_no_substrng_inlist_string.py
+++ python_data_structures/questions/find_no_substrng_inlist_string.py
@@ -7,3 +7,12 @@ str1.replace("is","")#NOT POSSIBLE AS STR1 IS IMMUTANT
 str2=str1.replace("is","")#strings are immutant , you cant append remove add replace any element in string str.replace will create a new string 
 print(str2)
 
+lst=[]#empty list
+size=int(input("enter no of names in list :"))
+lst=input("enter name separated by space : ").split()#-------->map is ued only to convert strings(lists of numbers) to integers if direct strings are input no need to use map
+#for input list via map-----> lst=input("enter name separated by space : ").split()
+if("tanish" in lst):
+    print("present")
+else:
+    print("not present")
+    
