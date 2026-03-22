d3fedfca6bb757e481ca0c2bc494bebdc764857b
Tanish <tanishsanghavi2@gmail.com>
2026-03-23 00:51:10 +0530

    feat: add negative slicing and skip-index logic

--- python_data_structures/strings/string_arrays.py
+++ python_data_structures/strings/string_arrays.py
@@ -9,3 +9,22 @@ print(name[1])
 print(name[0:])# 0: means 0 to size -1
 print(name[1:])
 print("negative slicing")
+
+'''
+negative slicing=start from end with index -1;
+print[name[-4:-1]]
+ 0  1  2  3  4 = h a r r y
+-5 -4 -3 -2 -1 = h a r r y
+'''
+name ='harry'
+
+print(name[-4:-1])
+print(name[1:4])
+
+#skipping indices[slicing]
+a='123456789'
+print(a[1:4:1])
+
+
+
+
