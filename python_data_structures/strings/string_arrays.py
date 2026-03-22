17af37a602f75811fbce9318a68ae471da740781
Tanish <tanishsanghavi2@gmail.com>
2026-03-23 00:51:10 +0530

    feat: add basic string slicing and positive indexing

---
+++ python_data_structures/strings/string_arrays.py
@@ -0,0 +1,11 @@
+name ="tanish sanghavi"
+
+nameshort=name[0:5]
+print(nameshort)
+nameshort=name[0:10]
+print(nameshort) 
+print(name[1]) 
+
+print(name[0:])# 0: means 0 to size -1
+print(name[1:])
+print("negative slicing")
