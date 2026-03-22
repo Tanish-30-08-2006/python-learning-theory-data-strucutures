9baa921feb283b4c1866971aa05ee5d6af1ce730
Tanish <tanishsanghavi2@gmail.com>
2026-03-23 00:51:05 +0530

    feat: add string length and search functions

---
+++ python_data_structures/strings/string_functions.py
@@ -0,0 +1,12 @@
+#ALL STRING FUNCTIONS CREATE A NEW STRING AND ORIGINAL STRING REMAINS SAME..
+# STRINGS ARE IMMUTABLE
+name='tanish sanghavi'
+
+print(len(name))
+print(name.startswith('ta'))
+print(name.endswith('sh'))
+print(name.endswith('Ta'))
+print(name.capitalize())
+
+index=name.find("ang")
+print(index)
