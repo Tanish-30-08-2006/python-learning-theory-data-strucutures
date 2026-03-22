e48a0ad332b4a078ba4ff0e6d0066551ee0b50d7
Tanish <tanishsanghavi2@gmail.com>
2026-03-22 22:02:52 +0530

    feat: add logic for finding the minimum element in a list

--- python_data_structures/questions/greatestelement.py
+++ python_data_structures/questions/greatestelement.py
@@ -12,3 +12,21 @@ for i in range(0,size,1):
 print(max) 
 print(max(lst))
 
+# --> min 
+size=int(input("enter no of elements :"))
+lst=[]#lists are mutant so we can append and delete
+for i in range(0,size,1):
+   lst.append(int(input(f"enter {i+1} element: ")))
+
+max1 =float('+inf')
+for i in range(0,size,1):
+   if lst[i]<max1:
+      max1=lst[i]
+print(max1)
+
+
+
+
+
+
+   
