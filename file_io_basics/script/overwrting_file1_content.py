be6ef6effad0da1c0ec7f0c206b266c5e81f1524
Tanish <tanishsanghavi2@gmail.com>
2026-04-10 23:55:56 +0530

    Finish overwrting_file1_content.py

--- file_io_basics/script/overwrting_file1_content.py
+++ file_io_basics/script/overwrting_file1_content.py
@@ -12,3 +12,16 @@ lines = [
     "Goal: Software Engineer\n",
     "Favorite Sport: Cricket\n",
     "Email: tanish@example.com\n",
+    "Phone: 9999999999\n",
+    "Country: India\n",
+    "Status: Learning Python File IO\n"
+]
+
+if os.path.exists(file_path):
+    with open(file_path,"w") as f: #overwrites...
+        f.writelines(lines) 
+#f.write accepts only a single string and returns number of characters written 
+#for list of strings or tuple we use f.writelines() which writes multiple string at once...
+print("\nlines appended successfully")
+
+
