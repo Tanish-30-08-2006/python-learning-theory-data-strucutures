576f2d7c2507a4796439be80ced453c191e9a096
Tanish <tanishsanghavi2@gmail.com>
2026-06-26 20:13:16 +0530

    add start of dictionary bar chart code

--- matplotlib/bar_chart/bar_chart_input_as_dictionary/script/code.py
+++ matplotlib/bar_chart/bar_chart_input_as_dictionary/script/code.py
@@ -26,32 +26,3 @@ def generate_bar_chart (dict , bar_chart_path):
     plt.savefig(bar_chart_path)
 
     #now show the graph
-    plt.show()
-
-    #close the image as multiple runs would overwrite the graph
-    plt.close()
-
-
-
-
-#first read the sample_data_.txt
-
-base_dir = os.path.dirname(__file__)
-sample_data_path = os.path.join(base_dir, "..", "data","sample_data_.txt")
-bar_chart_path = os.path.join(base_dir, "..", "data", "sample_data_bar_chart.png")
-
-lines=[]
-sample_dict = {} #dict is a reserved keyword , dict = {} is wrong 
-if os.path.exists(sample_data_path):
-    with open(sample_data_path,"r") as sdp:
-       lines = sdp.readlines() #breaks every line into a string untill \n comes 
-else:
-    print("NO SAMPLE DATA FOUND")
-
-for currline in lines:
-    parts = currline.split() #breaks again a string into two strings...
-    key = parts[0]        #key is a string 
-    value = int(parts[1]) #value is a integer (eg python 45)
-    sample_dict[key] = value
-
-generate_bar_chart(sample_dict,bar_chart_path)
