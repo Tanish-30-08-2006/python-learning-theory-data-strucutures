e2b6a61851f99c6e8a059e7a31710a032b6f147b
Tanish <tanishsanghavi2@gmail.com>
2026-06-26 20:13:14 +0530

    add start of basic bar chart code

--- matplotlib/bar_chart/bar_chart_basic_code_image_generation/script/bar_chart_basic_code.py
+++ matplotlib/bar_chart/bar_chart_basic_code_image_generation/script/bar_chart_basic_code.py
@@ -12,17 +12,3 @@ y=[2,5,7,6,5,4,1]
 plt.figure(figsize=(4,4))
 plt.bar(x,y, color= "green", edgecolor = "navy" , alpha=1) 
 
-
-plt.title("class perfomance analysis",fontsize=14)
-plt.xlabel("Grade Level" , fontsize=12)
-plt.ylabel("Number of Students" , fontsize=12)
-plt.grid(axis='y' , alpha = 0.3)
-
-plt.savefig(chart_image_path)
-plt.show()
-plt.close()
-
-
-
-
-
