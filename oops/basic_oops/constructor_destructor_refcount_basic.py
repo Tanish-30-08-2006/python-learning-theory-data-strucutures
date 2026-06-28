1ded3d3d5c91dd379c926c832775dba66f33927c
Tanish <tanishsanghavi2@gmail.com>
2026-06-28 12:02:06 +0530

    add basic logic to constructor and destructor file

--- oops/basic_oops/constructor_destructor_refcount_basic.py
+++ oops/basic_oops/constructor_destructor_refcount_basic.py
@@ -17,3 +17,22 @@ class student:
         print("constructor is called")
         self.name =name
     
+    def __del__(self):
+        print("destructor is called , ref count =0")
+        print(f"--- {self.name}'s memory address is being WIPED! ---")
+    
+
+
+# create an object(instance of class)
+s1 =student("alice") 
+# init is automatically called so student.init(s1,name) is called if function with same attributes self , name is there
+# then self.name = name is executed so s1 is a address of containng attributes ...
+
+print(f"References to Alice: {sys.getrefcount(s1)-1}")
+# s1 has memory address of attributes of s1 
+# Note: getrefcount is always +1 because the function itself 'refers' to it momentarily.
+
+#Create another pointer to the same address
+s2 = s1
+print(f"References to Alice after s2 = s1: {sys.getrefcount(s1) - 1}")
+# now s2 also points to same memory address that contains dictionary with self.__dict__['name'] = "alice"
