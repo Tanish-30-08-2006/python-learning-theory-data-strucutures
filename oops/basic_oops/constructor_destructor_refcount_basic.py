110a465f5963569ce4bb3f2a88509707aede6ec3
Tanish <tanishsanghavi2@gmail.com>
2026-06-28 12:02:07 +0530

    complete most of the constructor and destructor code

--- oops/basic_oops/constructor_destructor_refcount_basic.py
+++ oops/basic_oops/constructor_destructor_refcount_basic.py
@@ -36,3 +36,22 @@ print(f"References to Alice: {sys.getrefcount(s1)-1}")
 s2 = s1
 print(f"References to Alice after s2 = s1: {sys.getrefcount(s1) - 1}")
 # now s2 also points to same memory address that contains dictionary with self.__dict__['name'] = "alice"
+
+
+
+print(" Deleting s1")
+del s1
+print(f"References to Alice after deleting s1: {sys.getrefcount(s2) - 1}")
+
+# here memory address of s1 which contained dictionary self.__dict__['name']="alice" is still alive as s2 points to it
+# so destructor checks ref count of that address every time we call s1,use s1,del s1 ...or every student poinitng to alice it checks 
+# the the ref count of alice when we did del s1 it checks the ref count of self i.e where alice was stored
+# still ref count will be 1 so no destructor called
+
+print("deleting s2")
+del s2
+# here destructor is called ref count is 0 as no student pointer or variable s1 or s2 or s3 pointing to alice no destructor is called
+
+
+#----------------deleting objects (del s1)----------------------#
+
