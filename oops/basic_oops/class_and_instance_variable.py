adfddb326be01b3897951959ebea5b2975743311
Tanish <tanishsanghavi2@gmail.com>
2026-06-28 12:02:05 +0530

    add more logic to class and instance variable file

--- oops/basic_oops/class_and_instance_variable.py
+++ oops/basic_oops/class_and_instance_variable.py
@@ -6,3 +6,12 @@ class Student:
         self.name = name     #self.__dict__['name'] = name 
         self.marks = marks   #self.__dict__['marks'] = marks
 
+s1 = Student("Alice", 85)
+s2 = Student("Bob", 32)  #objects are created i.e instances of class
+
+print(s1.school_name)
+
+
+
+#--------------class and instance variables-------------
+'''
