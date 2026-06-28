a4587c5443dee57ba1c9898b89be82fd87790bdd
Tanish <tanishsanghavi2@gmail.com>
2026-06-28 12:02:06 +0530

    add init and self methods to basic class file

--- oops/basic_oops/class_object_self_init_basic.py
+++ oops/basic_oops/class_object_self_init_basic.py
@@ -17,3 +17,23 @@ class student:
             print(self)
             return line
         else:
+            line = f"{self.name} has failed"
+            print(self)
+            return line 
+
+s1 = student("alice" , 80)
+s2 = student("bob" ,30)
+
+print(s1.check_pass_fail())
+print(s2.check_pass_fail())
+
+#if we create an object for student def __init__ runs automatically when new student is created 
+
+
+'''
+what is self :this is placeholder it stores the students name and marks for every student
+(address of storage of attributes) for that student 
+self says, "Whichever specific student is being created right now, give them this name.
+self is essentially a pointer. When you create s1, Python allocates a chunk of memory to store Alice's data
+
+# def pass fail is a method it a function inside a class   
