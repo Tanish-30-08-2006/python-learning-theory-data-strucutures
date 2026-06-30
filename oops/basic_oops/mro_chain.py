ccf102c282c255f3d75c0282c4facb001be0717a
Tanish <tanishsanghavi2@gmail.com>
2026-06-30 15:03:33 +0530

    add more logic to mro chain file

--- oops/basic_oops/mro_chain.py
+++ oops/basic_oops/mro_chain.py
@@ -11,3 +11,16 @@ class GradStudent(Student):
     pass # This means 'do nothing/empty'
 
 # This WORKS because Python finds __init__ in the Parent
+s1 = GradStudent("Bob", 80)
+
+#python finds we havent used any method like s1.getgrade or something so it means we are calling init as method 
+#---------1
+# so here we havent called any method so it just means init is called so python searches '__init__' in s1 but currently s1 has 
+# no attributes not even ['name'] and ['marks'] ,in instances dictionary there are never methods ..
+
+#----------2
+# so we move to next link in method resolution order chain(mro chain)
+# type of s1 is gradstudent
+# class of s1 is gradstudent so gradstudent.__dict__['__init__'] is searched : result : FAIL
+
+#----------3
