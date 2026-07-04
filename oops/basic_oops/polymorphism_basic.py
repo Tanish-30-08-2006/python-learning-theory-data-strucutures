d335984e021c1ed8d32fac7fb4b8997787effd43
Tanish <tanishsanghavi2@gmail.com>
2026-07-04 23:41:45 +0530

    add basic logic to polymorphism file

--- oops/basic_oops/polymorphism_basic.py
+++ oops/basic_oops/polymorphism_basic.py
@@ -1,3 +1,7 @@
 class Student:
     def study(self):
         print("Reading basic textbooks...")
+
+class CSE_Student(Student):
+    def study(self):
+        print("Writing C code and checking memory leaks...")
