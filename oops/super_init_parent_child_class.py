ffaa3ac2f0ecfe3f67276b5534f0ff2166fbbefe
Tanish <tanishsanghavi2@gmail.com>
2026-07-04 23:41:47 +0530

    build the core parent and child functionality

--- oops/super_init_parent_child_class.py
+++ oops/super_init_parent_child_class.py
@@ -30,3 +30,19 @@ s1.display()      # Calling method from Parent
 s1.show_stipend() # Calling method from Child
 
 #-------------problem--------------------
+'''
+when we write init in child class it overwrites parents init and parents init never runs...
+parents logic of setting name and marks never runs and we cant use methods of student as    
+
+'''
+ #------------s1.dsiplay()--------------
+'''
+When you call a method like s1.display(), Python looks at the Child.
+ If it's not there, it uses that hidden link to check the Parent. This is called Delegation.
+
+ when we call s1.display()
+
+ first in memory block of s1 python searches for display in dict  of object s1 .s1.__dict__['display']
+ usually methods arent stored here name marks i.e attributes are stored here
+
+ second it sees type(s1) which is class scholarshipstudent so it searches for scholarshipstudent.__dict__['display']
