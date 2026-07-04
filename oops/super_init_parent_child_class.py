fae0da187ad1a0544e0cd8abf40d61bbe372d6b4
Tanish <tanishsanghavi2@gmail.com>
2026-07-04 23:41:46 +0530

    add super init methods to class file

--- oops/super_init_parent_child_class.py
+++ oops/super_init_parent_child_class.py
@@ -14,3 +14,19 @@ class student:
 
 #CHILD CLASS(derived class)
 class scholarshipstudent(student):
+    
+    def __init__(self , name , marks , stipend):
+        #here we must initialize parent(__init__) first then we initialise child i.e run childs init function
+        super().__init__(name , marks)
+        self.stipend = stipend
+        print(f"Child Init called for {self.name} with stipend {self.stipend}")
+    
+    def show_stipend(self):
+        print("calling method i.e a function from child which is not init")
+        print(f"stipend amount : {self.stipend}")
+
+s1 = scholarshipstudent("alice",95,1000)
+s1.display()      # Calling method from Parent
+s1.show_stipend() # Calling method from Child
+
+#-------------problem--------------------
